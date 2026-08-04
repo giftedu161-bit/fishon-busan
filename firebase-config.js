01278ea601c481f98b9008b7bec9270e7ae59ae3
giftedu161-bit <giftedu161-bit@users.noreply.github.com>
2026-08-04 11:39:47 +0900

    Connect app to Fishon Firebase project

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,8 @@
-// Firebase 콘솔에서 발급받은 웹 앱 설정값을 이 객체에 붙여넣으세요.
-// 설정 전에는 베타의 로컬 화면 흐름만 동작합니다.
-window.FISHON_FIREBASE_CONFIG = null;
+window.FISHON_FIREBASE_CONFIG = {
+  apiKey: 'AIzaSyDrAfKUpvFqX0kW57GVLunNpuQNseFf3Wc',
+  authDomain: 'fishon-busan.firebaseapp.com',
+  projectId: 'fishon-busan',
+  storageBucket: 'fishon-busan.firebasestorage.app',
+  messagingSenderId: '113931338567',
+  appId: '1:113931338567:web:f9c055b92fcaf19123c262'
+};
